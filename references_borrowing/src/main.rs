7ece5899d6098cc54cb9e947ffa449b8b3f5d1fc
Alejandro Montaner Roig <alroigcode@gmail.com>
2025-11-11 18:46:46 +0100

    Finish references and borrowing notes

--- references_borrowing/src/main.rs
+++ references_borrowing/src/main.rs
@@ -10,12 +10,56 @@ fn main() {
     let mut s = String::from("hello");
 
     let len = calculate_length(&s1); // s1 is not moved, we pass a reference to it,
-                                            // we can keep using s1 after this call
 
     println!("The length of '{s1}' is {len}.");
 
+    // This will cause a compile-time error because we are trying to
+    // modify a borrowed value through an immutable reference.
     //change_default_reference(&s1);
-    _change_mutable_reference(&mut s);
+
+    // This works because we are passing a mutable reference
+    change_mutable_reference(&mut s);
+
+    // Immutable references can be multiple
+    let or = String::from("hello");
+    let r1 = &or; // no problem
+    let r2 = &or; // no problem
+
+    // Mutable references are only allowed if there are no other references
+    let mut original = String::from("hello");
+    let r1 = &mut original;
+    //let r2 = &mute original; // ERROR: cannot borrow `original` as mutable more than once at a time
+
+
+    // We can use brackess to create a new scope, which allows us to have
+    // a mutable reference while the immutable references are no longer used.
+
+    let mut option = String::from("hello");
+    {
+        let op1 = &mut option; // no problem
+    } // op1 goes out of scope here, so we can make a new mutable reference
+    let op2 = &mut option; // no problem
+
+    // Combining mutable and immutable references
+    let mut combine = String::from("hello");
+
+    let c1 = &combine; // no problem
+    let c2 = &combine; // no problem
+    //let c3 = &mut combine; // ERROR: cannot borrow `combine` as mutable because it is also borrowed as immutable
+
+
+    let mut print = String::from("hello");
+
+    let p1 = &print; // no problem
+    let p2 = &print; // no problem
+    println!("{p1} and {p2}"); // Compiler can drop the immutable references here if last used
+    // After the last use of the immutable references, we can create a mutable reference
+    let p3 = &mut print; // no problem
+
+    // This line will cause an error if uncommented, because p1 and p2 are still in use.
+    // compiler assumed that previous print was the last use of p1 and p2
+    // so it allowed the creation o f a new mutable reference.
+    // println!("{p1}, {p2}, and {p3}");
 }
 
 // usize is an unsigned integer type that’s used for indexing, sizes,
@@ -30,9 +74,28 @@ fn calculate_length(s: &String) -> usize { // Takes a reference to a String and 
 // This function will cause a compile-time error because it tries to
 // modify a borrowed value through an immutable reference.
 fn change_default_reference(some_string: &String) {
+    // ERROR: cannot borrow `*some_string` as mutable, as it is behind a `&` reference
+    // some_string.push_str(", world");
+}
+
+// This function takes a mutable reference to a String and modifies it.
+fn change_mutable_reference(some_string: &mut String) {
     some_string.push_str(", world");
 }
 
-fn _change_mutable_reference(some_string: &mut String) {
-    some_string.push_str(", world");
-}
+// This function will not compile because it returns a reference to a value
+// that will go out of scope when the function ends.
+fn dangle() -> &String { // dangle returns a reference to a string
+    let s = String::from("hello"); // s is created here
+
+    &s // we return a reference to s
+} // Here, s goes out of scope and is dropped. Its memory is freed.
+    // Therefore, the reference we returned points to invalid memory.
+
+
+fn no_dangle() -> String {
+    let s = String::from("hello");
+
+    s // s is returned and moves out to the calling function
+} // Solution is to return the String directly. The ownership of the String
+  // is transferred to the calling function, so it remains valid.
