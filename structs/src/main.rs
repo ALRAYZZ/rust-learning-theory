71242aad29217bf8582ea687f9eff2deac970dbb
Alejandro Montaner Roig <alroigcode@gmail.com>
2025-11-12 16:28:41 +0100

    Add structs theory

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,71 @@
+mod tuple_struct;
+
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    // IMMUTABLE INSTANCE
+    // Rust doesn’t allow us to mark only certain fields as mutable
+    let user1 = User {
+        active: true,
+        username: String::from("usernametest"),
+        email: String::from("email"),
+        sign_in_count: 1,
+    };
+    // Not allowed, because user1 is immutable by default
+    //user1.email = String::from("anotheremail");
+
+    // MUTABLE INSTANCE
+    let mut user2 = User {
+        active: true,
+        username: String::from("usernametest2"), // struct owns the String
+        email: String::from("email2"),
+        sign_in_count: 1,
+    };
+    user2.email = String::from("anotheremail2");
+
+    // Creating instances from another instance
+    let user3 = User {
+        active: user2.active, // Implements Copy trait so this is fine
+        username: user2.username, // Moves ownership from user2 to user3
+        email: String::from("email3"), // New value
+        sign_in_count: user2.sign_in_count, // Implements Copy trait so this is fine
+    };
+
+    // USING STRUCT UPDATE SYNTAX
+    let user4 = User {
+        email: String::from("email4"),
+        ..user3  // fill in the rest from user3
+    };
+
+    // Problem, user2 got moved to user3.username so user2.username is no longer valid
+    //println!("{}", {user2.username});
+
+
+    tuple_struct::run();
+}
+
+// THIS CAN  BE SIMPLIFIED
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username: username,
+        email: email,
+        sign_in_count: 1,
+    }
+}
+
+// SIMPLIFIED VERSION - because the parameter names match the field names we can do this
+fn build_user_simplified(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,  // matching field name
+        email, // matching field name
+        sign_in_count: 1,
+    }
 }
---
+++ structs/src/tuple_struct.rs
@@ -0,0 +1,26 @@
+// Rust supports structs that look similar to tuples, called tuple structs.
+// Tuples structs are useful when you want to give the whole tuple a name and make the tuple's fields
+
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+// Unit-like structs without any fields, useful for generics
+struct AlwaysEqual;
+
+struct NotOwnData {
+    active: bool,
+    username: &str,
+    email: &str, // We need to use lifetimes here to ensure the references are valid
+    sign_in_count: u64,
+}
+
+pub fn run() {
+    let black = Color(1, 0, 0);
+    let origin = Point(10, 0, 0);
+
+    let origin_x = origin.0;
+
+    let subject = AlwaysEqual;
+
+    println!("{origin_x}");
+}
