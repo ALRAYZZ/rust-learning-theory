68ec4376b5f68b98dc595a428f1ac9a5da4b34b6
Alejandro Montaner Roig <alroigcode@gmail.com>
2025-11-13 12:08:42 +0100

    Add enums theory - Create option enum

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,10 +1,56 @@
+mod option_enum;
+
 enum IpAddrKind {
     V4,
     V6,
 }
 
+enum IpAddrInfo {
+    V4(u8, u8, u8, u8), // We can also store associated data directly in the enum variants
+    V6(String), // We are de facto making a constructor that takes a String argument
+}
+
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+enum Message { // Enums can contain data as well as structs.
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message  {
+    fn call (&self) {
+        // method body would be defined here
+    }
+}
+
 
 fn main () {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    let ip = IpAddrInfo::V4(127, 0, 0, 1);
+    let ip_6 = IpAddrInfo::V6(String::from("::1"));
+
+
+    let m  = Message::Write(String::from("hello"));
+    m.call();
+
+
+}
+
+fn route (ip_kind: IpAddrKind) {
+
 }
