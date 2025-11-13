250f5c098d92ae7c1a3159260f016f29a252de4c
Alejandro Montaner Roig <alroigcode@gmail.com>
2025-11-13 16:19:24 +0100

    Creation restaurant library crate

--- modules/src/main.rs
+++ modules/src/main.rs
@@ -1,3 +1,10 @@
+use crate::garden::vegetables::Asparagus;
+
+pub mod garden;
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let plant = Asparagus {};
+    println!("I planted {:?}", plant);
 }
