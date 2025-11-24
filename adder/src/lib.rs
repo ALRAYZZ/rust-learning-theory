cd6a53b530859978a256a4db72c6ec344863df24
Alejandro Montaner Roig <alroigcode@gmail.com>
2025-11-24 14:38:37 +0100

    Create testing project/adder

---
+++ adder/src/lib.rs
@@ -0,0 +1,8 @@
+#[cfg(test)]
+mod tests {
+    #[test] // Indicates that this is a test function
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4); // Asserts that one is equal to the other
+    }
+}
