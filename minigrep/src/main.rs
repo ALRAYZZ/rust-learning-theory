78b18ac93d675c78ca9c322612128faa613781b9
Alejandro Montaner Roig <alroigcode@gmail.com>
2025-11-26 15:25:09 +0100

    Improved return type of Config constructor, for proper user messaging

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,15 +1,18 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect(); // we annotate the type because collect() is ambiguous
 
-    let config = Config::new(&args);
+    // Unwrap_or_else is a method that either unwraps the Result if it's Ok
+    // or calls the provided closure if it's Err
+    let config = Config::build(&args).unwrap_or_else((|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    }));
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
-    let contents = fs::read_to_string(config.file_path)
+    let contents = fs::read_to_string(&config.file_path)
         .expect("Something went wrong reading the file");
 
     println!("With text:\n{contents}");
@@ -21,10 +24,17 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    // we use &'static str because the error message we return has a static lifetime
+    // is the common approach for simple error handling in Rust
+    // means the string doesn't come from a variable or computed at runtime
+    // its hardcoded into the program, specifically in this function
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
