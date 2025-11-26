47c028ff29366d8d1cd502178d34acc43aef92e9
Alejandro Montaner Roig <alroigcode@gmail.com>
2025-11-26 15:57:03 +0100

    Further separation of concers and error handling

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect(); // we annotate the type because collect() is ambiguous
@@ -12,10 +13,24 @@ fn main() {
         process::exit(1);
     }));
 
-    let contents = fs::read_to_string(&config.file_path)
-        .expect("Something went wrong reading the file");
+
+    // Different pattern to handle errors because the run function does not return
+    // a value that we want to unwrap, since it returns Ok(()) on success
+    // so we just match on the Result directly
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
+
+// Box<dyn Error> is a trait object that can represent any error type
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    // ? operator returns to the caller the error so the caller can decide how to handle it
+    let contents = fs::read_to_string(config.file_path)?;
 
     println!("With text:\n{contents}");
+
+    Ok(())
 }
 
 struct Config {
