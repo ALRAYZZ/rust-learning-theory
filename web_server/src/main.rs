99fc362be8349fa8b8b67b71a58b10af83772128
Alejandro Montaner Roig <alroigcode@gmail.com>
2025-12-09 16:35:38 +0100

    Refactor using if expression to avoid duplication of code when defining different responses based on user requests

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -23,32 +23,26 @@ fn handle_connection(mut stream: TcpStream) {
         .unwrap()
         .unwrap();
 
-    if request_line == "GET / HTTP/1.1" {
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("hello.html").unwrap();
-        let length = contents.len();
+    let (status_line, filename) =
+    // Rust can use if expressions to return values, so in this case we can populate
+    // the tuple with the appropriate values based on the condition
+        if request_line == "GET / HTTP/1.1" {
+            ("HTTP/1.1 200 OK", "hello.html")
+        } else {
+            ("HTTP/1.1 404 NOT FOUND", "404.html")
+        };
 
-        let response = format!(
-            "{status_line}\r\n\
-            Content-Length: {length}\r\n\r\n\
-            {contents}"
-        );
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
 
-        // write_all needs a mutable reference to stream as parameter as it changes the stream state
-        stream.write_all(response.as_bytes()).unwrap();
-    } else {
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("404.html").unwrap();
-        let length = contents.len();
+    let response = format!(
+        "{status_line}\r\n\
+        Content-Length: {length}\r\n\r\n\
+        {contents}"
+    );
 
-        let response = format!(
-            "{status_line}\r\n\
-            Content-Length: {length}\r\n\r\n\
-            {contents}"
-        );
-
-        stream.write_all(response.as_bytes()).unwrap();
-    }
+    // write_all needs a mutable reference to stream as parameter as it changes the stream state
+    stream.write_all(response.as_bytes()).unwrap();
 
 
 }
