74b56dbb8443b9dd1d2753b149342dd5c47bda0f
Alejandro Montaner Roig <alroigcode@gmail.com>
2025-12-10 19:20:33 +0100

    Solved strings exercises rustlings

--- rustlings/exercises/09_strings/strings3.rs
+++ rustlings/exercises/09_strings/strings3.rs
@@ -1,13 +1,18 @@
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string.
+    input.trim()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+    // In order to use the + to concatenate strings, the first string needs to be owned.
+    // so we need to convert the &str to a String first.
+    input.to_owned() + " world!"
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    input.replace("cars", "balloons")
 }
 
 fn main() {
