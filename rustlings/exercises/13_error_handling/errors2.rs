41f85f509ab7c1cb9a66ba30122dee9440b31b5a
Alejandro Montaner Roig <alroigcode@gmail.com>
2025-12-11 17:26:13 +0100

    Solved error_handling exercises rustlings

--- rustlings/exercises/13_error_handling/errors2.rs
+++ rustlings/exercises/13_error_handling/errors2.rs
@@ -21,7 +21,10 @@ fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let cost_per_item = 5;
 
     // TODO: Handle the error case as described above.
-    let qty = item_quantity.parse::<i32>();
+    // The ? operator is syntactic sugar for error propagation.
+    // If parse method returns Err, the function will return that Err immediately.
+    // If it returns Ok, the value inside Ok is assigned to qty.
+    let qty = item_quantity.parse::<i32>()?;
 
     Ok(qty * cost_per_item + processing_fee)
 }
