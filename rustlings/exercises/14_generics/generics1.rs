384651e00f28d6f9d418638f60d7db409aa8db1e
Alejandro Montaner Roig <alroigcode@gmail.com>
2025-12-11 17:40:14 +0100

    Solved generics exercises rustlings

--- rustlings/exercises/14_generics/generics1.rs
+++ rustlings/exercises/14_generics/generics1.rs
@@ -6,7 +6,9 @@ fn main() {
     // TODO: Fix the compiler error by annotating the type of the vector
     // `Vec<T>`. Choose `T` as some integer type that can be created from
     // `u8` and `i8`.
-    let mut numbers = Vec::new();
+    // It needs to be i16 because neither i8 or u8 can hold all values of the other type.
+    // so we need a bigger type that can hold both.
+    let mut numbers = Vec::<i16>::new();
 
     // Don't change the lines below.
     let n1: u8 = 42;
--- rustlings/exercises/14_generics/generics2.rs
+++ rustlings/exercises/14_generics/generics2.rs
@@ -1,12 +1,16 @@
 // This powerful wrapper provides the ability to store a positive integer value.
 // TODO: Rewrite it using a generic so that it supports wrapping ANY type.
-struct Wrapper {
-    value: u32,
+struct Wrapper<T> {
+    value: T,
 }
 
 // TODO: Adapt the struct's implementation to be generic over the wrapped value.
-impl Wrapper {
-    fn new(value: u32) -> Self {
+// Here we need to specify first on the impl block that we are implementing for a generic type T.
+// like hey this entire block of methods is generic and the placeholder for the type is T.
+// Then the target of the implementation is Wrapper<T> which means we are implementing methods
+// for the struct Wrapper that holds a value of type T.
+impl<T> Wrapper<T> {
+    fn new(value: T) -> Self {
         Wrapper { value }
     }
 }
