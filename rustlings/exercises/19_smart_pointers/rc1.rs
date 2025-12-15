9eb31c0a6481e946f6652b3d9d4fe931895a8b22
Alejandro Montaner Roig <alroigcode@gmail.com>
2025-12-15 18:35:15 +0100

    Solved smart pointers exercises rustlings

--- rustlings/exercises/19_smart_pointers/rc1.rs
+++ rustlings/exercises/19_smart_pointers/rc1.rs
@@ -34,6 +34,11 @@ fn main() {
 mod tests {
     use super::*;
 
+    // The use of RC is when different parts of the code need to own the same data.
+    // In this case, multiple planets need to own the Sun.
+    // So using Reference Counting (Rc) is appropriate here.
+    // We keep track of how many planets are orbiting the Sun
+    // We store the Sun in a Rc, and each planet gets a clone of that Rc,
     #[test]
     fn rc1() {
         let sun = Rc::new(Sun);
@@ -60,17 +65,17 @@ mod tests {
         jupiter.details();
 
         // TODO
-        let saturn = Planet::Saturn(Rc::new(Sun));
+        let saturn = Planet::Saturn(Rc::clone(&sun));
         println!("reference count = {}", Rc::strong_count(&sun)); // 7 references
         saturn.details();
 
         // TODO
-        let uranus = Planet::Uranus(Rc::new(Sun));
+        let uranus = Planet::Uranus(Rc::clone(&sun));
         println!("reference count = {}", Rc::strong_count(&sun)); // 8 references
         uranus.details();
 
         // TODO
-        let neptune = Planet::Neptune(Rc::new(Sun));
+        let neptune = Planet::Neptune(Rc::clone(&sun));
         println!("reference count = {}", Rc::strong_count(&sun)); // 9 references
         neptune.details();
 
@@ -92,12 +97,15 @@ mod tests {
         println!("reference count = {}", Rc::strong_count(&sun)); // 4 references
 
         // TODO
+        drop(earth);
         println!("reference count = {}", Rc::strong_count(&sun)); // 3 references
 
         // TODO
+        drop(venus);
         println!("reference count = {}", Rc::strong_count(&sun)); // 2 references
 
         // TODO
+        drop(mercury);
         println!("reference count = {}", Rc::strong_count(&sun)); // 1 reference
 
         assert_eq!(Rc::strong_count(&sun), 1);
